04e8d061c4d2d3443f5cf738a62dc613e11061bc
Gustavo Casanova <gustavo.casanova@nicebots.com>
2020-05-14 02:50:53 -0300

    Update Arduino Pro Mini pinout-Deek Robot.pdf, ArduinoProMini-DeekRobot.jpg, and 5 more files...

---
+++ software/sw-tests/tst-07/include/tst-07.h
@@ -0,0 +1,16 @@
+//
+// tst-07.h
+//
+
+#ifndef _TST_07_H_
+#define _TST_07_H_
+
+#include <Arduino.h>
+#include <Wire.h>
+
+// Prototipe
+void setup(void);
+void loop(void);
+void receiveEvent(int);
+
+#endif // _TST_07_H_

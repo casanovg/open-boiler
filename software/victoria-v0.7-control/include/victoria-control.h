30b83e85e90e488d8e71a2d4d506f9f11a2b8be2
Gustavo Casanova <gustavo.casanova@gmail.com>
2020-06-15 12:43:47 -0300

    Update victoria-control.h

--- software/victoria-v0.7-control/include/victoria-control.h
+++ software/victoria-v0.7-control/include/victoria-control.h
@@ -61,80 +61,80 @@
 #define DHW_SETTING_STEPS 12        /* DHW setting potentiometer steps */
 #define CH_SETTING_STEPS 12         /* CH setting potentiometer steps */
 
-// Flame detector (mini-pro pin 2 - input)
+// Flame detector (pro-mini pin 2 - input)
 #define FLAME_DDR DDRD
 #define FLAME_PIN PIN2
 #define FLAME_PINP PIND
 #define FLAME_PORT PORTD
-// Exhaust fan (mini-pro pin 3 - output)
+// Exhaust fan (pro-mini pin 3 - output)
 #define FAN_DDR DDRD
 #define FAN_PIN PIN3
 #define FAN_PINP PIND
 #define FAN_PORT PORTD
-// Spark igniter (mini-pro pin 4 - output)
+// Spark igniter (pro-mini pin 4 - output)
 #define SPARK_DDR DDRD
 #define SPARK_PIN PIN4
 #define SPARK_PINP PIND
 #define SPARK_PORT PORTD
-// Valve security (mini-pro pin 5 - output)
+// Valve security (pro-mini pin 5 - output)
 #define VALVE_S_DDR DDRD
 #define VALVE_S_PIN PIN5
 #define VALVE_S_PINP PIND
 #define VALVE_S_PORT PORTD
-// Valve 1 (mini-pro pin 6 - output)
+// Valve 1 (pro-mini pin 6 - output)
 #define VALVE_1_DDR DDRD
 #define VALVE_1_PIN PIN6
 #define VALVE_1_PINP PIND
 #define VALVE_1_PORT PORTD
-// Valve 2 (mini-pro pin 7 - output)
+// Valve 2 (pro-mini pin 7 - output)
 #define VALVE_2_DDR DDRD
 #define VALVE_2_PIN PIN7
 #define VALVE_2_PINP PIND
 #define VALVE_2_PORT PORTD
-// Valve 3 (mini-pro pin 8 - output)
+// Valve 3 (pro-mini pin 8 - output)
 #define VALVE_3_DDR DDRB
 #define VALVE_3_PIN PIN0
 #define VALVE_3_PINP PINB
 #define VALVE_3_PORT PORTB
-// Water pump (mini-pro pin 9 - output)
+// Water pump (pro-mini pin 9 - output)
 #define PUMP_DDR DDRB
 #define PUMP_PIN PIN1
 #define PUMP_PINP PINB
 #define PUMP_PORT PORTB
-// Overheat thermostat (mini-pro pin 10 - input)
+// Overheat thermostat (pro-mini pin 10 - input)
 #define OVERHEAT_DDR DDRB
 #define OVERHEAT_PIN PIN2
 #define OVERHEAT_PINP PINB
 #define OVERHEAT_PORT PORTB
-// Domestic Hot Water request (mini-pro pin 11 - input)
+// Domestic Hot Water request (pro-mini pin 11 - input)
 #define DHW_RQ_DDR DDRB
 #define DHW_RQ_PIN PIN3
 #define DHW_RQ_PINP PINB
 #define DHW_RQ_PORT PORTB
-// Central Heating request (mini-pro pin 12 - input)
+// Central Heating request (pro-mini pin 12 - input)
 #define CH_RQ_DDR DDRB
 #define CH_RQ_PIN PIN4
 #define CH_RQ_PINP PINB
 #define CH_RQ_PORT PORTB
-// Led UI (mini-pro pin 13 - onboard led - output)
+// Led UI (pro-mini pin 13 - onboard led - output)
 #define LED_UI_DDR DDRB
 #define LED_UI_PIN PIN5
 #define LED_UI_PINP PINB
 #define LED_UI_PORT PORTB
-// DHW potentiometer (mini-pro pin A0 - intput)
+// DHW potentiometer (pro-mini pin A0 - intput)
 #define DHW_POT ADC0
-// CH potentiometer (mini-pro pin A1 - intput)
+// CH potentiometer (pro-mini pin A1 - intput)
 #define CH_POT ADC1
-// System potentiometer (mini-pro pin A2 - intput)
+// System potentiometer (pro-mini pin A2 - intput)
 #define SYS_POT ADC2
-// Air-flow sensor (mini-pro pin A3 - intput) Board 4-6 left to right
+// Air-flow sensor (pro-mini pin A3 - intput) Board 4-6 left to right
 #define AIRFLOW_DDR DDRC
 #define AIRFLOW_PIN PIN3
 #define AIRFLOW_PINP PINC
 #define AIRFLOW_PORT PORTC
-// DHW temperature sensor (mini-pro pin A6 - intput)
+// DHW temperature sensor (pro-mini pin A6 - intput)
 //#define DHW_TEMP ADC6
-// CH temperature sensor (mini-pro pin A7 - intput)
+// CH temperature sensor (pro-mini pin A7 - intput)
 //#define CH_TEMP ADC7
 #define ADC_MIN 0
 #define ADC_MAX 1023
